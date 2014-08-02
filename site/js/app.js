9a7eef72aa6f2d22154af4870da8e31439abe6b4
米田 匡史 <pillows916@gmail.com>
2014-08-02 14:50:33 +0900

    車種の追加（addCar）機能完了

--- site/js/app.js
+++ site/js/app.js
@@ -2,8 +2,7 @@ var app = app || {};
 
 $(function () {
 	var cars = [
-		{carKind: "A", maxCapacity: 4, rentalFee: 6000},
-		{carKind: "B", maxCapacity: 5, rentalFee: 7000}
+		{carKind: "A", maxCapacity: 4, rentalFee: 6000}
 	];
 
 	new app.CarsView(cars);
--- site/js/views/carsView.js
+++ site/js/views/carsView.js
@@ -1,17 +1,42 @@
 var app = app || {};
 
-	app.CarsView = Backbone.View.extend({
-        el: '#cars',
+app.CarsView = Backbone.View.extend({
+    el: '#cars',
 
-        initialize: function(initialCars) {
-        	this.collection = new app.CarList(initialCars);
-        	this.render();
-        },
+    initialize: function(initialCars) {
+    	this.collection = new app.CarList(initialCars);
+    	this.render();
+    	this.listenTo(this.collection, 'add', this.renderCar)
+    },
 
-		render: function() {
-			this.collection.each(function(item) {
-				var carView = new app.CarView({model: item});
-				this.$el.append(carView.render().el);
-			}, this);
-		}
-	});
+    events: {
+    	'click #add': 'addCar'
+    },
+
+    addCar: function(e) {
+    	e.preventDefault();
+
+    	var formData = {};
+
+    	$('#addCar div p').children('input').each(function(i, el) {
+    		console.log($(el).val());
+    		if ($(el).val() != '') {
+    			formData[el.id] = $(el).val();
+    		}
+    	})
+
+    	this.collection.add(new app.Car(formData));
+    },
+
+	render: function() {
+		this.collection.each(function(item) {
+			this.renderCar(item);
+		}, this);
+	},
+
+	renderCar: function(item) {
+		var carView = new app.CarView({model: item});
+		this.$el.append(carView.render().el);
+	}
+
+});
