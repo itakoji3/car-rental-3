87a277fd23b59a308d91744841a6ae8ff3449fdc
itakoj3 <itakoj3@gmail.com>
2014-08-02 17:13:41 +0900

    Support delete

--- site/js/views/carView.js
+++ site/js/views/carView.js
@@ -5,8 +5,18 @@ var app = app || {};
 
 		template: _.template($('#carTemplate').html()),
 
+        events: {
+        	'click .delete': 'deleteCar'
+        },
+
 		render: function() {
 			this.$el.html(this.template(this.model.toJSON()));
 			return this;
+		},
+
+		deleteCar: function() {
+			this.model.destroy();
+			this.remove();
 		}
+
 	});
